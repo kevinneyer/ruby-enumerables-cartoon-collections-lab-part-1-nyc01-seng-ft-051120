49f39fb1ef0a80861c5752881d4edfe4f2ddbbc0
Kevin Neyer <kevinneyer@gmail.com>
2020-04-14 15:03:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
 array.each_with_index do |value, index|
-  index = array.length 
-  print"#{index + 1}. #{value}"
+  print"#{index + 1}. #{value }"
 end 
 end
